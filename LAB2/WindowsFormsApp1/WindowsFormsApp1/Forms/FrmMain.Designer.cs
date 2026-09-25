241e701efe0757262a88fd6c0a453822ec699302
Nhan Phi Phố <1250080142@sv.hcmunre.edu.vn>
2026-09-25 21:03:10 +0700

    Add files via upload

---
+++ LAB2/WindowsFormsApp1/WindowsFormsApp1/Forms/FrmMain.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace QuanLyThuVien.Forms
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1Title = new System.Windows.Forms.Label();
+            this.btnDanhMuc = new System.Windows.Forms.Button();
+            this.btnSach = new System.Windows.Forms.Button();
+            this.btnDocGia = new System.Windows.Forms.Button();
+            this.btnMuonTra = new System.Windows.Forms.Button();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb1Title
+            // 
+            this.lb1Title.AutoSize = true;
+            this.lb1Title.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1Title.Location = new System.Drawing.Point(198, 23);
+            this.lb1Title.Name = "lb1Title";
+            this.lb1Title.Size = new System.Drawing.Size(522, 46);
+            this.lb1Title.TabIndex = 0;
+            this.lb1Title.Text = "HỆ THỐNG QUẢN LÝ THƯ VIỆN";
+            this.lb1Title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnDanhMuc
+            // 
+            this.btnDanhMuc.Location = new System.Drawing.Point(94, 104);
+            this.btnDanhMuc.Name = "btnDanhMuc";
+            this.btnDanhMuc.Size = new System.Drawing.Size(223, 56);
+            this.btnDanhMuc.TabIndex = 1;
+            this.btnDanhMuc.Text = "Quản Lí Danh Mục";
+            this.btnDanhMuc.UseVisualStyleBackColor = true;
+            this.btnDanhMuc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSach
+            // 
+            this.btnSach.Location = new System.Drawing.Point(94, 233);
+            this.btnSach.Name = "btnSach";
+            this.btnSach.Size = new System.Drawing.Size(223, 54);
+            this.btnSach.TabIndex = 2;
+            this.btnSach.Text = "Quản Lý Sách";
+            this.btnSach.UseVisualStyleBackColor = true;
+            this.btnSach.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnDocGia
+            // 
+            this.btnDocGia.Location = new System.Drawing.Point(94, 371);
+            this.btnDocGia.Name = "btnDocGia";
+            this.btnDocGia.Size = new System.Drawing.Size(223, 54);
+            this.btnDocGia.TabIndex = 3;
+            this.btnDocGia.Text = "Độc giả và thẻ";
+            this.btnDocGia.UseVisualStyleBackColor = true;
+            // 
+            // btnMuonTra
+            // 
+            this.btnMuonTra.Location = new System.Drawing.Point(538, 104);
+            this.btnMuonTra.Name = "btnMuonTra";
+            this.btnMuonTra.Size = new System.Drawing.Size(223, 56);
+            this.btnMuonTra.TabIndex = 4;
+            this.btnMuonTra.Text = "Quản Lý Mượn - Trả Sách";
+            this.btnMuonTra.UseVisualStyleBackColor = true;
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Location = new System.Drawing.Point(538, 233);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(223, 54);
+            this.btnThongKe.TabIndex = 5;
+            this.btnThongKe.Text = "Thống Kê";
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(538, 371);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(223, 54);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 647);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnThongKe);
+            this.Controls.Add(this.btnMuonTra);
+            this.Controls.Add(this.btnDocGia);
+            this.Controls.Add(this.btnSach);
+            this.Controls.Add(this.btnDanhMuc);
+            this.Controls.Add(this.lb1Title);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Quản Lý Thư Viện";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb1Title;
+        private System.Windows.Forms.Button btnDanhMuc;
+        private System.Windows.Forms.Button btnSach;
+        private System.Windows.Forms.Button btnDocGia;
+        private System.Windows.Forms.Button btnMuonTra;
+        private System.Windows.Forms.Button btnThongKe;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
---
+++ LAB2/WindowsFormsApp1/WindowsFormsApp1/Forms/FrmSach.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp1.Forms
+{
+    partial class FrmSach
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Quản Lý Đầu Sách";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmSach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmSach";
+            this.Text = "FrmSach";
+            this.Load += new System.EventHandler(this.FrmSach_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
